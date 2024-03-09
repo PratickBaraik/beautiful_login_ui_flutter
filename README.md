712776aeb559ca95bf62030b54b2dcb2d3c8cf78
PratickB111 <animeNerd.int@protonmail.com>
2024-03-09 19:59:39 +0530

    Adding flutter files of beautiful login ui single scroll view activity or page

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a modern beautiful login UI made using flutter
---
+++ android/app/src/main/kotlin/com/example/flutter_responsive_login_ui/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.beautiful_login_u
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
